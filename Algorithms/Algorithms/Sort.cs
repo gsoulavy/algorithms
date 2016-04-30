82c14f1fae48f73ac989ba66f8401ac82429bb97
Gabor Soulavy <g.soulavy@gmail.com>
2016-04-30 23:37:20 +0100

    Add merge sort to Sort class of Algorithms

---
+++ Algorithms/Algorithms/Sort.cs
@@ -0,0 +1,50 @@
+﻿namespace Algorithms
+{
+    public static class Sort
+    {
+        public static int[] MergeSort(int[] sequence)
+        {
+            var halfLength = sequence.Length / 2;
+            var left = new int[halfLength];
+            var right = new int[sequence.Length - halfLength];
+
+            for (var i = 0; i < sequence.Length; i++)
+            {
+                if (i < halfLength)
+                {
+                    left[i] = sequence[i];
+                }
+                else
+                {
+                    right[i - halfLength] = sequence[i];
+                }
+            }
+
+            if ((left.Length + right.Length) > 2)
+            {
+                left = MergeSort(left);
+                right = MergeSort(right);
+            }
+
+            var leftCount = 0;
+            var rightCount = 0;
+            for (var i = 0; i < sequence.Length; i++)
+            {
+                var l = (leftCount < left.Length) ? left[leftCount] : new int?();
+                var r = (rightCount < right.Length) ? right[rightCount] : new int?();
+
+                if (l < r || r == null)
+                {
+                    if (l != null) sequence[i] = l.Value;
+                    leftCount++;
+                }
+                else if (r < l || l == null)
+                {
+                    sequence[i] = r.Value;
+                    rightCount++;
+                }
+            }
+            return sequence;
+        }
+    }
+}
---
+++ Algorithms/AlgorithmsTest/SortTest.cs
@@ -0,0 +1,22 @@
+﻿using Algorithms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmsTest
+{
+    [TestClass]
+    public class SortTest
+    {
+        [TestMethod]
+        public void MergeSortTest()
+        {
+            //Arrange
+            var input = new[] { 5, 2, 4, 6, 1, 3, 9, 8, 7, 0 };
+
+            //Act
+            var result = Sort.MergeSort(input);
+
+            //Assert
+            CollectionAssert.AreEqual(result, new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+        }
+    }
+}
