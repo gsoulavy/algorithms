2f7412b4a01aec116a31d12a1adacf181eb0fa3e
Gabor Soulavy <g.soulavy@gmail.com>
2016-05-29 12:14:31 +0100

    Add fibonacci sequence algorithm to the solution

---
+++ Algorithms/Algorithms/Fibonacci.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms
+{
+    public static class Fibonacci
+    {
+        private static readonly Dictionary<int, double> _memo = new Dictionary<int, double>();
+
+        public static double Calculate(int n)
+        {
+            var memoResult = _memo.Get(n);
+            if (memoResult != null) return memoResult.Value;
+            var result = n < 2 ? (double)n : Calculate(n - 1) + Calculate(n - 2);
+            _memo[n] = result;
+            return result;
+        }
+
+        private static double? Get(this Dictionary<int, double> memo, int index)
+        {
+            if (memo.ContainsKey(index)) return memo[index];
+            return null;
+        }
+    }
+
+    public static class DictionaryExtension
+    {
+        public static double Fibonacci(this Dictionary<int, double> dictionary, int n)
+        {
+            double? memoResult = null;
+            if (dictionary.ContainsKey(n)) memoResult = dictionary[n];
+            if (memoResult != null) return memoResult.Value;
+            var result = n < 2 ? (double)n : dictionary.Fibonacci(n - 1) + dictionary.Fibonacci(n - 2);
+            dictionary[n] = result;
+            return result;
+        }
+
+        public static Dictionary<int, double> FibonacciSequence(this Dictionary<int, double> dictionary, int n)
+        {
+            dictionary.Fibonacci(n);
+            return dictionary.OrderBy(d => d.Key).ToDictionary(d => d.Key, (d) => d.Value);
+        }
+    }
+}
---
+++ Algorithms/AlgorithmsTest/FibonacciTest.cs
@@ -0,0 +1,111 @@
+﻿using Algorithms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace AlgorithmsTest
+{
+    [TestClass]
+    public class FibonacciTest
+    {
+        [TestMethod]
+        public void Fibonacci_GetZeroElement()
+        {
+            //Arrange
+
+            //Act
+            var result = Fibonacci.Calculate(0);
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Fibonacci_GetFirstElement()
+        {
+            //Arrange
+
+            //Act
+            var result = Fibonacci.Calculate(1);
+
+            //Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Fibonacci_GetSecondElement()
+        {
+            //Arrange
+
+            //Act
+            var result = Fibonacci.Calculate(2);
+
+            //Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Fibonacci_GetThirdElement()
+        {
+            //Arrange
+
+            //Act
+            var result = Fibonacci.Calculate(3);
+
+            //Assert
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Fibonacci_GetFourthElement()
+        {
+            //Arrange
+
+            //Act
+            var result = Fibonacci.Calculate(4);
+
+            //Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void Fibonacci_GetFifthElement()
+        {
+            //Arrange
+
+            //Act
+            var result = Fibonacci.Calculate(5);
+
+            //Assert
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void Fibonacci_GetSixthElement()
+        {
+            //Arrange
+
+            //Act
+            var result = Fibonacci.Calculate(6);
+
+            //Assert
+            Assert.AreEqual(8, result);
+        }
+
+        [TestMethod]
+        public void Fibonacci_GetCollectionOfFibonaccy()
+        {
+            //Arrange
+            var criteria = new Dictionary<int, double>();
+
+            for (var i = 0; i < 10; i++)
+            {
+                criteria[i] = Fibonacci.Calculate(i);
+            }
+            //Act
+            var result = new Dictionary<int, double>().FibonacciSequence(9);
+
+            //Assert
+            CollectionAssert.AreEqual(criteria, result);
+        }
+    }
+}
